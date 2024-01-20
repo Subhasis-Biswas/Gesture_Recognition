9079e884e6f7d1aeecd736542ac9bb03f93318bb
Subhasis Biswas <subhasisbisw@iisc.ac.in>
2024-01-20 18:16:33 +0530

    Now restricted to run on Linux based systems only. Additional support maybe added later.

--- GestureProj/__init__.py
+++ GestureProj/__init__.py
@@ -1,6 +1,11 @@
 #load a python script as a module
 import os
 import sys
+import platform
+
+if platform.system()!='Linux':
+    print("Current version is not supported on "+platform.system()+". Consider checking for updates.")
+    exit()
 
 module_path = os.path.abspath(__file__)
 module_root = os.path.dirname(module_path)
